fd063e4d3fa103c49a1e71d756f7aff7e4ee0931
assassincreedv <assassincreedv@hotmail.com>
2019-07-12 17:40:26 -0400

    commit 1

---
+++ eCommerceMVC/Repository/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using eCommerceMVC.Models;
+using eCommerceMVC.Repository;
+
+namespace eCommerceMVC.Repository
+{
+    public class ProductRepository:GenericRepository<Product>, IProductRepository
+    {
+        private JooleEntities _dbcontext;
+        private DbSet<Product> dbset;
+        public ProductRepository(JooleEntities dbcontext) : base(dbcontext)
+        {
+            this._dbcontext = dbcontext;
+            this.dbset = _dbcontext.Set<Product>();
+        }
+    }
+}
---
+++ eCommerceMVC/UoW/UoW.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eCommerceMVC.Models;
+using eCommerceMVC.Repository;
+
+namespace eCommerceMVC.UoW
+{
+    public class UnitofWork:IDisposable
+    {
+        private JooleEntities dbcontext;
+        private static UserRepository userrepository;
+        private static ProductRepository productrepository;
+        public UnitofWork(JooleEntities dbcontext) {
+            this.dbcontext = dbcontext;
+        }
+        
+        public UserRepository UserRepository
+        {
+            get {
+                if (userrepository == null) {
+                    userrepository = new UserRepository(dbcontext);
+                }
+                return userrepository;
+            }
+        }
+        public ProductRepository ProductRepository
+        {
+            get {
+                if (productrepository == null) {
+                    productrepository = new ProductRepository(dbcontext);
+                }
+                return productrepository;
+            }
+        }
+        public void Save() {
+            dbcontext.SaveChanges();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing) {
+            if (!this.disposed)
+                if (disposing)
+                    dbcontext.Dispose();
+            this.disposed = true;
+        }
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+       
+    }
+}
